2e08f6ca57a2f6f491f12990c9372de2512cdee2
Amitgadodiya1 <amitgadodiya12@gmail.com>
2025-04-14 17:34:33 +0530

    update compose file and added header in frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,5 +2,13 @@ import axios from "axios";
 
 const BASE_URL = "http://127.0.0.1:5000/api";
 
-export const fetchModels = () => axios.get(`${BASE_URL}/models`);
-export const fetchModelById = (id) => axios.get(`${BASE_URL}/models/${id}`);
+const headers = {
+  headers: {
+    "x-api-key": process.env.REACT_APP_API_KEY,
+  },
+};
+
+export const fetchModels = () => axios.get(`${BASE_URL}/models`, headers);
+
+export const fetchModelById = (id) =>
+  axios.get(`${BASE_URL}/models/${id}`, headers);
